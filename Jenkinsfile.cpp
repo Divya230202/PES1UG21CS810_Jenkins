03881f5bb33c56861461df60b75a45425ae7757e
Divya <111753723+Divya230202@users.noreply.github.com>
2023-02-19 16:45:27 +0530

    Create Jenkinsfile.cpp

---
+++ Jenkinsfile.cpp
@@ -0,0 +1,29 @@
+pipeline{
+          agent any
+          stages{
+            stage('Build'){
+                 steps{
+                     sh 'g++ -c main/hello.cpp'
+                     sh 'g++ -o PES1UG21CS810 main/hello.cpp'
+                     echo 'Build stage successfull'
+                 }
+             }
+             stage('Test'){
+                  steps{
+                      sh './PES1UG21CS810'
+                      echo 'Test stage successful'
+                   }
+              }
+             stage('Deploy'){
+                  steps{
+
+                     echo 'Deployment Successful'
+                   }
+              }
+           }
+          post{
+             failure{
+                 echo 'Pipeline Failed'
+              }
+          }
+       }
